13510539dadff326b6f1600a436e2cdd9bab449b
Kyle-Rhys Hatcher <khatcher@airlinehyd.com>
2023-05-01 16:02:26 -0400

    Demonstrate missing text in Shell on RELEASE mode vs DEBUG configuration

---
+++ CollectionViewFormattedStringSlowness/Controls/FlyoutFooter.xaml.cs
@@ -0,0 +1,9 @@
+namespace Controls;
+
+public partial class FlyoutFooter : ContentView
+{
+	public FlyoutFooter()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CollectionViewFormattedStringSlowness/Controls/FlyoutHeader.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Controls;
+
+public partial class FlyoutHeader : ContentView
+{
+	public FlyoutHeader()
+	{
+		InitializeComponent();
+	}
+}
+
